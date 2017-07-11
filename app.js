c03c5f897c65450758de05eda78d9437e5e2c5ed
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-07-11 12:15:00 +0200

    update GA

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+var downloads = $(".download");
+var reset = $("#reset");
+var p = $(".paragraph");
+
+downloads.on("click", function() {
+	var el = $(this);
+
+	el.hide(); // el.css("display", "none") OU el.css({"display": "none"})
+
+	// Tag Google Analytics => https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+	ga('send', 'event', 'Boutton', 'click', el.text());
+});
+
+reset.on("click", function() {
+	downloads.show();
+	p.slideDown();
+});
+
+p.on("click", function() {
+	$(this).slideUp(500);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var title = $("#title");
 var button = $("#button");
 
@@ -11,13 +49,13 @@ button.on("click", function(){
         }
 });
 
-button.on("mouseover", function() {
-	title.innerHTML = "Mouseover!!!";
+button.on("mouseenter", function() {
+	title.text("Mouseover!!! <h1>ezfzef</h1>");
 });
 
-button.on("mouseout", function() {
-        title.innerHTML = "Mouseout!!!";
+button.on("mouseleave", function() {
+        title.text("Mouseout!!!");
 });
 
 var p = $(".paragraph");
-p.css("backgroundColor", "red");
+p.css({"backgroundColor": "red", color: "deeppink", fontSize: "40px"});
